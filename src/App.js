9cb733dd8c30ab5243c23184487d2e9a3e822fa3
Umer Anis <umeranis24@gmail.com>
2021-01-15 21:34:21 +0500

    increament and decreament

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const state = useState();
+  const [count , setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  };
+
+  const DecNum = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+      <h1> {count} </h1>
+      <button onClick={IncNum}>Increament</button>
+      <button onClick={DecNum}>Decreament</button>
+      </div>
+  )
 }
 
 export default App;
